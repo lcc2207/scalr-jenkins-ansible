ba75ab374bdd56c8f8256fe9000e583194280de0
lcc2207 <lynn@fidmail.com>
2018-03-21 14:07:46 -0500

    fix errors

--- scalr-jenkins.sh
+++ scalr-jenkins.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-apt-get update; apt-get install -y python-pip
+apt-get update; apt-get install -y python-pip git
 
 # install ansible
 pip install ansible
 
+# clone repo
+git clone https://github.com/lcc2207/scalr-jenkins-ansible.git
+cd scalr-jenkins-ansible/
+
 # pull down required ansible roles
 ansible-galaxy install --roles-path . geerlingguy.jenkins
 
+echo Pa55w0rd > ./vault
+
 # execute the jenkins install playbook
-ansible-playbook -i localhost jenkins.yml
+ansible-playbook -i localhost jenkins.yml --vault-id ./vault
